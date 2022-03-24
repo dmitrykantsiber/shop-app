797d05c109e99ed111f8fff14b11fb241f015eb5
dimas <kantsiber505@ukr.net>
2022-03-24 18:59:09 +0200

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import HomePage from "./pages/homepage/homepage.component";
+import {Switch, Route } from "react-router-dom";
 
-
+const HatsPage = () => {
+  return (
+    <div>
+      <h1> HATS PAGE</h1>
+    </div>
+  );
+};
 function App() {
   return (
- 
-     <HomePage />
-
+    <div>
+      <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
